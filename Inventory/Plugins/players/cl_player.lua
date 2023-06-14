e6245b77eb025532272a245bfa295b57ee5744a2
Musiker15 <info@musiker15.de>
2023-06-14 21:25:52 +0200

    players

---
+++ Inventory/Plugins/players/cl_player.lua
@@ -0,0 +1,14 @@
+RegisterNetEvent('inventory:openPlayerInventory', function(pid, receiveWeapons)
+    OpenInventory({type = 'player', id = pid, title = Locales.playerTitle, weight = true, receiveWeapons = receiveWeapons, delay = 1000})
+end)
+
+isPlayerDead = false
+AddEventHandler('esx:onPlayerDeath', function() isPlayerDead = true end)
+AddEventHandler('esx:onPlayerSpawn', function(spawn) isPlayerDead = false end)
+
+AddEventHandler("inventory:open", function()
+    if isPlayerDead then -- IsPedDeadOrDying(PlayerPedId())
+        CloseInventory()
+    end
+end)
+exports('CloseInventory', CloseInventory) -- exports.inventory:CloseInventory()
---
+++ Inventory/Plugins/players/readme.md
@@ -0,0 +1,7 @@
+Hey guys, 
+
+this is a fix for when the player is dead that he can't open the inventory.
+
+there is also a new export to close the Inventory.
+
+Have fun :)
